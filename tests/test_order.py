a4f44e52c2fc179c8f2e7276ddf326b5a30ab84d
Irina Kozlova <irina.kozlova@vk.team>
2026-06-05 16:59:23 +0300

    additional fix for removing time.sleep

--- tests/test_order.py
+++ tests/test_order.py
@@ -2,41 +2,16 @@ import allure
 import pytest
 from pages.main_page import MainPage
 from pages.order_page import OrderPage
+from data import TestData
 
 
 class TestOrder:
     """Тесты для позитивного сценария заказа самоката"""
     
-    # Тестовые данные
-    ORDER_DATA = [
-        {
-            "name": "Иван",
-            "surname": "Петров",
-            "address": "ул. Ленина, 10",
-            "metro": "Сокольники",
-            "phone": "+79001234567",
-            "date": "25.12.2024",
-            "rental_period": "сутки",
-            "color": "black",
-            "comment": "Позвонить за час"
-        },
-        {
-            "name": "Анна",
-            "surname": "Сидорова",
-            "address": "пр. Мира, 5",
-            "metro": "Комсомольская",
-            "phone": "+79109876543",
-            "date": "30.12.2024",
-            "rental_period": "трое суток",
-            "color": "grey",
-            "comment": "Домофон не работает"
-        }
-    ]
-    
     @allure.story("Оформление заказа")
     @allure.title("Позитивный сценарий заказа через {button_position} кнопку")
     @pytest.mark.parametrize("button_position", ["top", "bottom"])
-    @pytest.mark.parametrize("order_data", ORDER_DATA)
+    @pytest.mark.parametrize("order_data", TestData.ORDER_DATA)
     def test_positive_order_flow(self, driver, button_position, order_data):
         """Проверка полного флоу заказа самоката"""
         main_page = MainPage(driver)
